ed8a1a1adc798e7a15a2ae84ec9a3be24111f9ef
matiaschaves01 <matiaschaves01@gmail.com>
2023-01-18 16:32:12 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`server listening on ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"));
+});
+
+app.get("/views/register.html", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/views/login.html", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
